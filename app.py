02ad1f1b6d484fde081821a2cdd22a2c198a484a
AmlMoawadElshora <ineedtorun127@gmail.com>
2024-01-31 00:27:26 +0200

    files upload

--- app.py
+++ app.py
@@ -1,38 +1,81 @@
+from flask import Flask, render_template, request, jsonify
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
 from PIL import Image
+import os
+from werkzeug.utils import secure_filename  # Import secure_filename to prevent filename attacks
 
-
-
+app = Flask(__name__)
 
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-max_length=16
-num_beams=4
-gen_kwargs={"max_length":max_length,"num_beams":num_beams}
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
 
 def predict_caption(image_paths):
-    images=[]
+    images = []
     for image_path in image_paths:
-        i_image=Image.open(image_path)
-        if i_image.mode !="RGB":
-            i_image=i_image.convert(mode="RGB")
+        i_image = Image.open(image_path)
+        if i_image.mode != "RGB":
+            i_image = i_image.convert(mode="RGB")
         images.append(i_image)
 
-    pixel_values=feature_extractor(
-        images=images,return_tensors="pt").pixel_values
-    pixel_values=pixel_values.to(device)
-    
-    output_ids=model.generate(pixel_values,**gen_kwargs)
+    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
 
-    preds=tokenizer.batch_decode(output_ids,skip_special_tokens=True)
-    preds=[pred.strip() for pred in preds]
-    print("Final Caption is: ",preds)
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
     return preds
-predict_caption(['Test1.jpeg'])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    file_path = f"uploads/{secure_filename(file.filename)}"
+    file.save(file_path)
+
+    captions = predict_caption([file_path])
+
+    return jsonify({'caption': captions[0]})
+
+
+@app.route('/predictApi', methods=["POST"])
+def api():
+    try:
+        if 'file' not in request.files:
+            return jsonify({'error': "Please try again. The Image doesn't exist"})
+        image = request.files.get('file')
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(image.filename))
+        image.save(file_path)
+
+        predictions = predict_caption([file_path])
+        predicted_caption = predictions[0]
+        return jsonify({'prediction': predicted_caption})
+    except Exception as e:
+        return jsonify({'Error': str(e)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
